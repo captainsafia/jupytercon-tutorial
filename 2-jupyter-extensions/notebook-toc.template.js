fd1a0011127a65d9c32b597385938c1abff302d7
Safia Abdalla <safia@safia.rocks>
2017-08-22 05:51:25 -0500

    Fix template and completed extensions

---
+++ 2-jupyter-extensions/notebook-toc.template.js
@@ -0,0 +1,17 @@
+define(['base/js/namespace', 'base/js/utils', 'jquery'], function(Jupyter, utils, $) {
+    function create_toc_text() {}
+
+    function load_toc_cell(toc_cell, toc_text) {}
+
+    function create_toc() {}
+
+    function refresh_toc() {}
+
+    function place_toc_button() {}
+
+    function load_ipython_extension() {}
+
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
